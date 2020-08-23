c63d8768c1b9967aa28d7bec59522f6a2c24fd63
ouchiminh <39828763+ouchiminh@users.noreply.github.com>
2020-08-23 23:04:35 +0900

    code

---
+++ test/GraphBasicTest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using csGraph;
+using System.Collections.Generic;
+using System.Linq;
+
+[TestClass]
+public class GraphBasicTest
+{
+    IEnumerable<int> GetNodes()
+    {
+        for (int i = 1; i < 10; ++i) yield return i;
+        yield break;
+    }
+    [TestMethod]
+    public void TestNode()
+    {
+        Graph<int> G = new Graph<int>();
+        G.AddNode(0);
+        G.AddNodes(GetNodes());
+        Assert.AreEqual(10, G.nodes.Count());
+    }
+    [TestMethod]
+    public void TestEdgeUndirected()
+    {
+        Graph<int> G = new Graph<int>();
+        G.isDirected = false;
+        Assert.IsTrue(G.AddEdge(1, 2));
+        Assert.IsFalse(G.AddEdge(0, 0));
+        Assert.IsFalse(G.AddEdge(2, 1));
+        Assert.AreEqual(1, G.edges.Count());
+        Assert.AreEqual(2, G.nodes.Count());
+    }
+    [TestMethod]
+    public void TestEdgeDirected()
+    {
+        Graph<int> G = new Graph<int>();
+        G.isDirected = true;
+        Assert.IsTrue(G.AddEdge(1, 2));
+        Assert.IsFalse(G.AddEdge(0, 0));
+        Assert.IsTrue(G.AddEdge(2, 1));
+        Assert.AreEqual(2, G.edges.Count());
+        Assert.AreEqual(2, G.nodes.Count());
+    }
+    [TestMethod]
+    public void TestDegree()
+    {
+        Graph<int> G = new Graph<int>();
+        G.isDirected = true;
+        G.AddEdge(1, 2);
+        G.AddEdge(0, 0);
+        G.AddEdge(2, 1);
+
+        Assert.AreEqual(2, G.CountDegree(1));
+        Assert.AreEqual(1, G.CountInDegree(1));
+        Assert.AreEqual(1, G.CountOutDegree(1));
+        Assert.AreEqual(2, G.CountDegree(2));
+        Assert.AreEqual(1, G.CountInDegree(2));
+        Assert.AreEqual(1, G.CountOutDegree(2));
+    }
+    [TestMethod]
+    public void TestNodeDelete()
+    {
+        Graph<int> G = new Graph<int>();
+        G.isDirected = true;
+        G.AddEdge(1, 2);
+        G.AddEdge(0, 0);
+        G.AddEdge(2, 1);
+
+        G.DeleteNode(1);
+
+        Assert.AreEqual<int>(1, G.nodes.Count());
+        Assert.AreEqual<int>(0, G.edges.Count());
+    }
+}
---
+++ test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b2ad199e-c6f8-4dd0-ae07-b77baa7ca63b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
